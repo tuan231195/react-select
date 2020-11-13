027df9a876e2b055f481b71da62cd224f060cd73
Tuan Nguyen <tuannguyen@nine.com.au>
2020-11-13 12:54:17 +1100

    docs(docs): add docs

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # React Select
 
 > React Select Implementation with Tailwind css styling
+
+## Storybook
+
+https://tuan231195.github.io/react-select/
--- src/components/Select/types.ts
+++ src/components/Select/types.ts
@@ -7,16 +7,37 @@ export type SelectOption = {
 };
 
 export type SelectProps = {
+	/**
+	 * the list of options
+	 */
 	options: { value: any; option: any }[];
+	/**
+	 * The current value of the select input
+	 */
 	value?: any;
+	/**
+	 * Customizable components
+	 */
 	components?: {
 		singleValue?: React.FC<{ contentEditable?: React.ReactNode }>;
 		option?: React.FC<{ item: SelectOption }>;
 		multiValue?: React.FC<{ item: SelectOption }>;
 	};
+	/**
+	 * Input placeholder
+	 */
 	placeholder?: string;
+	/**
+	 * Whether to allow multiple selections
+	 */
 	multi?: boolean;
+	/**
+	 * Whether to show loading
+	 */
 	loading?: boolean;
+	/**
+	 * On Change callback
+	 */
 	onChange?: ({
 		option,
 		value,
@@ -26,16 +47,40 @@ export type SelectProps = {
 		value: SelectOption['value'];
 		isRemove?: boolean;
 	}) => void;
+	/**
+	 * Convert option to a string
+	 */
 	optionToText?: (option: SelectOption) => string;
+	/**
+	 * Whether to close on select
+	 */
 	closeOnSelect?: boolean;
+	/**
+	 * Whether to show clear button
+	 */
 	showClear?: boolean;
+	/**
+	 * On user search callback
+	 */
 	onSearch?: (search: string) => void;
+	/**
+	 * Allow item's add
+	 */
 	allowAdd?: boolean;
+	/**
+	 * On add callback
+	 */
 	onAdd?: (newOption: SelectOption) => void;
 };
 
 export type AsyncSelectProps = Omit<SelectProps, 'options'> & {
+	/**
+	 * Query for options
+	 */
 	asyncOptions: (search: string) => Promise<SelectOption[]>;
 	options?: SelectOption[];
+	/**
+	 * debounce in seconds
+	 */
 	debounce?: number;
 };
